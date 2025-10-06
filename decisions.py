e60a734d3a83a2eb11d5aa47a10383d1b20797df
Raul Moldovan <raul.m.engineer@gmail.com>
2025-10-06 21:27:09 +0300

    feat:Implemented first conditional logic in decisions.py

---
+++ decisions.py
@@ -0,0 +1,8 @@
+def user_name(Raul):
+    user_input = input("Please enter your name: ")
+    if user_input == Raul:
+        print("Hello,Raul")
+    else:
+        print("Who are you?")
+
+user_name("Raul")
